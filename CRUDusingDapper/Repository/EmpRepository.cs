66894d4a6695d889c1c8953c7f5be722d41d6f8f
pankaj-shah-2016 <pankaj.pankajrshah@gmail.com>
2024-10-09 18:26:30 +0530

    Display Done

---
+++ CRUDusingDapper/Repository/EmpRepository.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CRUDusingDapper.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
+using System.Data;
+
+namespace CRUDusingDapper.Repository
+{
+    public class EmpRepository
+    {
+        public SqlConnection con;
+        //To Handle connection related activities      
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["SqlConn"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+        //To Add Employee details      
+        public void AddEmployee(EmpModel objEmp)
+        {
+
+            //Additing the employess      
+            try
+            {
+                connection();
+                con.Open();
+                con.Execute("AddNewEmpDetails", objEmp, commandType: CommandType.StoredProcedure);
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+        //To view employee details with generic list       
+        public List<EmpModel> GetAllEmployees()
+        {
+            try
+            {
+                connection();
+                con.Open();
+                IList<EmpModel> EmpList = SqlMapper.Query<EmpModel>(
+                                  con, "GetEmployees").ToList();
+                con.Close();
+                return EmpList.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //To Update Employee details      
+        public void UpdateEmployee(EmpModel objUpdate)
+        {
+            try
+            {
+                connection();
+                con.Open();
+                con.Execute("UpdateEmpDetails", objUpdate, commandType: CommandType.StoredProcedure);
+                con.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+        //To delete Employee details      
+        public bool DeleteEmployee(int Id)
+        {
+            try
+            {
+                DynamicParameters param = new DynamicParameters();
+                param.Add("@EmpId", Id);
+                connection();
+                con.Open();
+                con.Execute("DeleteEmpById", param, commandType: CommandType.StoredProcedure);
+                con.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Log error as per your need       
+                throw ex;
+            }
+        }
+    }
+}
